3acd3ac626451dc8573dae33fd476f58e77aba00
Yakir Gagnon <12.yakir@gmail.com>
2024-12-12 16:04:09 +0100

    wip

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,10 @@
 using BinDeps
 
-basedir = dirname(@__FILE__)
+basedir = @__DIR__()
+
+const urls = (linux = "https://exiftool.org/Image-ExifTool-13.07.tar.gz",
+              apple = "https://exiftool.org/ExifTool-13.07.pkg",
+              windows = "https://exiftool.org/exiftool-13.07_64.zip")
 
 program = "exiftool"
 
