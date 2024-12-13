9f901c435dc406fdf5e17d0e80e5077e1c4b7f33
Yakir Gagnon <12.yakir@gmail.com>
2024-12-13 11:57:08 +0100

    testing

--- src/functions.jl
+++ src/functions.jl
@@ -7,8 +7,8 @@
 # end
 
 function get_recording_datetime(file)
-    txts = strip.(split(read(`$exiftool -T -AllDates -n $file`, String), '\t'))
-    # txts = strip.(split(read(`exiftool -T -AllDates -n $file`, String), '\t'))
+    # txts = strip.(split(read(`$exiftool -T -AllDates -n $file`, String), '\t'))
+    txts = strip.(split(read(`exiftool -T -AllDates -n $file`, String), '\t'))
     dts = [DateTime(txt[1:19], DateFormat("yyyy:mm:dd HH:MM:SS")) for txt in txts if length(txt) > 18]
     if isempty(dts)
         return missing
