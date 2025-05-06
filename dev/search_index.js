e4cb96c301910cfd8b1c4887400453dab9407146
Documenter.jl <documenter@juliadocs.github.io>
2025-05-06 14:48:29 +0000

    build based on ae36eae

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Fromage","category":"page"},{"location":"#Fromage","page":"Home","title":"Fromage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Fromage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Fromage]","category":"page"}]
+[{"location":"#Fromage","page":"Home","title":"Fromage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Fromage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
